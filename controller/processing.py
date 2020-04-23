48bc9d29c1a758ff32fc51d22f136b9de56ed770
nikola <nlukic@usc.edu>
2020-04-23 06:35:57 +0100

    Fix bug

--- controller/processing.py
+++ controller/processing.py
@@ -4,7 +4,7 @@ from configs import *
 def loadMappings(mappingFile = 'keycodes.json', shiftOnFile = 'shift_on.nfo'):
     with open(mappingFile) as json_file:
 		mappings = json.load(json_file)
-        
+
     with open(shiftOnFile) as set_file:
 		shiftOn = set(set_file.read().split(" "))
 		shiftOn.remove('')
@@ -20,25 +20,25 @@ class EventProcessor:
         self.hidFile = hidFile
         if calibrate:
             self.calibrate_pointer()
-        super().__init__()
+        #super().__init__()
 
-    def write_report(self, report):
+    def write_report(self, report)
     	with open(self.hidFile, 'rb+') as fd:
-    		fd.write(report.encode())	
+    		fd.write(report.encode())
 
     # Move the pointer to upper-left corner.
     def calibrate_pointer(self):
     	self.currentX = 0
     	self.currentY = 0
 
-        self.logging.debug("CALIBRATING")
+        self.logging.debug("Calibrating!")
 
         # TODO: Make this move diagonal.
     	for i in range (0, WIDTH):
-    		write_report(LEFT_STEP_REPORT)
+    		self.write_report(LEFT_STEP_REPORT)
     	for i in range (0, HEIGHT):
-    		write_report(UP_STEP_REPORT)
-    
+    		self.write_report(UP_STEP_REPORT)
+
     def createReportFromKey(self, key):
         if key.islower():
 	    	string = NULL_CHAR * 2 + chr(self.mappings[key]) + NULL_CHAR * 5
@@ -55,35 +55,35 @@ class EventProcessor:
         return string
 
     def execute_keyboard_command(self, key):
-        self.logging.debug("PRESSED %s", key)
+        self.logging.debug("Pressed key: %s", key)
         string = self.createReportFromKey(key)
         self.write_report(string)
 
 
-    def execute_mouse_command(self, xPercentage, yPercentage, click):	
-	    newX = int(round(xPercentage * WIDTH))
-	    newY = int(round(yPercentage * HEIGHT))
+    def execute_mouse_command(self, xPercentage, yPercentage, click):
+	self.logging.debug("Exec mouse cmd: %.3f, %.3f, %s", xPercentage, yPercentage, click)
+	newX = int(round(xPercentage * WIDTH))
+	newY = int(round(yPercentage * HEIGHT))
 
-	    diffX = int(abs(newX - currentX))
-	    diffY = int(abs(newY - currentY))
+	diffX = int(abs(newX - self.currentX))
+	diffY = int(abs(newY - self.currentY))
 
-	    xMove = RIGHT_STEP_REPORT
-	    if newX <= currentX:
-	    	xMove = LEFT_STEP_REPORT
-    
-	    yMove = DOWN_STEP_REPORT
-	    if newY <= currentY:
-	    	yMove = UP_STEP_REPORT
-    
-	    for i in range(0, diffX):
-	    	write_report(xMove)
+	xMove = RIGHT_STEP_REPORT
+	if newX <= self.currentX:
+		xMove = LEFT_STEP_REPORT
 
-	    for i in range(0, diffY):
-	    	write_report(yMove)
+	yMove = DOWN_STEP_REPORT
+	if newY <= self.currentY:
+		yMove = UP_STEP_REPORT
 
-	    if click:
-	    	write_report(TOUCH_REPORT)
+	for i in range(0, diffX):
+		self.write_report(xMove)
 
-    
-	    self.currentY = newY
-	    self.currentX = newX
+	for i in range(0, diffY):
+		self.write_report(yMove)
+
+	if click:
+		self.write_report(TOUCH_REPORT)
+
+	self.currentY = newY
+	self.currentX = newX
